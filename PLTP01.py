2068d82887b6c3af68dc8a9dab033c9dab78553d
José Ramos <36642448+a73855@users.noreply.github.com>
2021-03-22 23:55:20 +0000

    Update 1 to 4 done
    Update 1 to 4 done

--- PLTP01.py
+++ PLTP01.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[10]:
 
 
 import re
 import json
 
 
-# In[13]:
+# In[11]:
 
 
 
@@ -39,17 +39,18 @@ def buscarecebendoCaps(ER,fraseFonte):
       print("Não encontrado")    
 
 
-# In[14]:
+# In[12]:
 
 
 ## Teste de Leitura
 # read file
 with open('inscritos-form.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
 
 # parse file
-obj=json.loads(data)
+obj = json.loads(data)
 obj 
+
 
 
 # # Processador de Inscritos numa atividade Desportiva
@@ -67,36 +68,39 @@ obj
 
 # a) imprimir o nome (convertido para maiúsculas) de todos os concorrentes que se inscrevem como 'Individuais' e são de 'Valongo'.
 
-# In[10]:
+# In[63]:
 
 
 
 ## a)
 # read file
 with open('inscritos-form.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
 
 # parse file
-obj=json.loads(data)
+obj = json.loads(data)
 obj = json.loads(data)
 
-data=obj.pop('inscritos')
+data = obj.pop('inscritos')
 
 content = data#open('inscritos-form.json', encoding="utf-8")
 
 
-# In[23]:
+# In[56]:
 
 
 for i in range(len(content)):
-    linha=content[i]
+    linha = content[i]
+    
     #print(str(linha))
-    teste =re.search(r'(?i:Valongo)',str(linha))
-    individual=re.search(r'(?i:Individual)',str(linha))
-    if(teste and individual): 
-        print('---------Dados de Inscrito---------')
-        buscarecebendoCaps(r'\'nome\': \'([^\']*)\'',str(linha))
-        print()
+    teste = re.search(r'(?i:Valongo)',linha['morada'])
+    if(teste):
+        individual = re.search(r'(?i:Individual)',linha['equipa'])
+    
+        if(individual): 
+            print('---------Dados de Inscrito---------')
+            print(linha['nome'].upper())
+            print()
         
    
 
@@ -105,130 +109,175 @@ for i in range(len(content)):
 # seja 'Paulo' ou 'Ricardo', desde que usem o GMail.
 # 
 
-# In[16]:
+# In[100]:
 
 
 ## b)
 
 # read file
 with open('inscritos-form.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
 
 # parse file
-obj=json.loads(data)
+obj = json.loads(data)
 obj = json.loads(data)
 
-data=obj.pop('inscritos')
+data = obj.pop('inscritos')
 
 content = data#open('inscritos-form.json', encoding="utf-8")
 
 
-# In[19]:
+# In[101]:
 
 
 for i in range(len(content)):
-    linha=content[i]
+    linha = content[i]
     #print(str(linha))
-    paulo =re.search(r'(?i:Paulo)',str(linha)) ## aqui posso especificar que deve estar dentro do nome
-    ricardo=re.search(r'(?i:Ricardo)',str(linha))
-    gmail=re.search(r'(?i:@gmail)',str(linha))
+    paulo = re.search(r'(?i:Paulo)',linha['nome']) ## aqui posso especificar que deve estar dentro do nome
+    ricardo = re.search(r'(?i:Ricardo)',linha['nome'])
+    if((paulo or ricardo)):
+        gmail = re.search(r'(?i:@gmail)',linha['email'])
     
-    if((paulo or ricardo)and gmail): 
-        print('---------Dados de Utilizador---------')
-        buscarecebendoCaps(r'\'nome\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'email\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'prova\': \'([^\']*)\'',str(linha))
-        print()
+        if( gmail): 
+            print('---------Dados de Utilizador---------')
+            print(linha['nome'].upper())
+            print(linha['email'])
+            print(linha['prova'])
+            print()
    
 
 
 #  c) imprimir toda a informação dos atletas da equipe "TURBULENTOS".
 
-# In[ ]:
+# In[107]:
 
 
 
 ## c)
 # read file
 with open('inscritos-form.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
 
 # parse file
-obj=json.loads(data)
+obj = json.loads(data)
 obj = json.loads(data)
 
-data=obj.pop('inscritos')
+data = obj.pop('inscritos')
 
-content = data#open('inscritos-form.json', encoding="utf-8")
+content = data
+#open('inscritos-form.json', encoding="utf-8")
 
 
-# In[22]:
+# In[109]:
 
 
 for i in range(len(content)):
-    linha=content[i]
-    #print(str(linha))
-    turb =re.search(r'(TURBULENTOS)',str(linha)) ## turbulentos vs os turbolentos <- ver isto
+    linha = content[i]
+    
+    turb = re.search(r'(TURBULENTOS)',linha['equipa']) ## turbulentos vs os turbolentos <- ver isto
     
     
     if(turb ): 
         print('---------Dados de Utilizador---------')
-        buscarecebendoCaps(r'\'nome\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'dataNasc\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'morada\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'email\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'prova\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'escalao\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'equipa\': \'([^\']*)\'',str(linha))
+        print(linha['nome'])
+        print(linha['dataNasc'])
+        print(linha['morada'])
+        print(linha['email'])
+        print(linha['prova'])
+        print(linha['escalao'])
+        print(linha['equipa'])
         print()
 
 
 # d) imprimir a lista dos escalões por ordem alfabética e para cada um indicar quantos atletas estão inscritos nesse escalão.
 
-# In[24]:
+# In[104]:
 
 
 
 ## d)
 # read file
 with open('inscritos-form.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
 
 # parse file
-obj=json.loads(data)
+obj = json.loads(data)
 obj = json.loads(data)
 
-data=obj.pop('inscritos')
+data = obj.pop('inscritos')
 
 content = data#open('inscritos-form.json', encoding="utf-8")
 
 
-# In[26]:
+# In[ ]:
+
+
+
+
+
+# In[105]:
 
 
 #primeiro ciclo vai buscar todos os escalões e ordena 
 # 2º ciclo vai a lista e pega nos nomes dos do dado escalão
 # - escalão : 
 # tendo a estrutura imprime
+d = dict()
+for a in content:
+    if a['escalao'] not in d:
+        d[a['escalao']] = 1
+    else:
+        d[a['escalao']]+=1
 
-for i in range(len(content)):
-    linha=content[i]
-    #print(str(linha))
-    escalao =re.search(r'escalao',str(linha)) 
-   
-    if(turb ): 
-        print('---------Dados de Utilizador---------')
-        buscarecebendoCaps(r'\'nome\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'dataNasc\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'morada\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'email\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'prova\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'escalao\': \'([^\']*)\'',str(linha))
-        buscarecebendo(r'\'equipa\': \'([^\']*)\'',str(linha))
-        print()
+print(d)
 
 
-# e) gerar uma página HTML com a lista das equipes inscritas em qualquer prova, indicando o seu nome e o número dos atletas que a constituem e que se inscreveram pelo menos uma vez numa prova; essa lista deve estar ordenada por ordem decrescente do número de atletas; além disso, cada equipe deve ter um link para outra página HTML com a informação que achar interessante sobre cada atleta indicando as provas em que cada um participou.
+# e) gerar uma página HTML com a lista das equipes inscritas em qualquer prova, indicando o seu nome e o número dos atletas que a constituem e que se inscreveram pelo menos uma vez numa prova; 
+# 
+# essa lista deve estar ordenada por ordem decrescente do número de atletas; 
+# 
+# além disso, cada equipe deve ter um link para outra página HTML com a informação que achar interessante sobre cada atleta indicando as provas em que cada um participou.
+
+# In[ ]:
+
+
+# 1 º equipas + numero de atletas
+# ordenar lista por ordem decrescente de n de atletas
+# Link para outra pagina html
+    # ATLETAS DE 1 EQUIPA (nome) com informação de provas em que está inscrito
+
+
+# In[126]:
+
+
+
+## e)
+# read file
+with open('inscritos-form.json', 'r') as myfile:
+    data = myfile.read()
+# parse file
+obj = json.loads(data)
+content = obj.pop('inscritos')
+
+
+# In[123]:
+
+
+d = dict()
+for a in content:
+    if a['equipa'] not in d:
+        d[a['equipa']] = 1
+    else:
+        d[a['equipa']]+=1
+#d
+
+
+# In[122]:
+
+
+for w in sorted(d, key=d.get, reverse=True):
+    print(w, d[w])
+
 
 # In[ ]:
 
