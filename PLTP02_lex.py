67d8ce753665b6af030bf113c51c98618e57f0f7
Francisco Peixoto <thtr.irubra@gmail.com>
2021-05-28 22:51:59 +0100

    added stuff
    array
    for
    
    ++
    --

--- PLTP02_lex.py
+++ PLTP02_lex.py
@@ -1,17 +1,19 @@
 import sys
 import ply.lex as lex
 
-tokens=['INT','ID','NUM','IF','IFELSE','OR','AND','BIGEQUAL','LESSEREQUAL','EQUALS','WHILE','DO','STDIN','STDOUT','COMMENT','NOTEQUALS','TRUE','FALSE','DEFINE','FUNC', 'CALL','ELSE','PAL']
+tokens=['INT','ID','NUM','ARRAY','IF','IFELSE','OR','AND','BIGEQUAL','LESSEREQUAL','EQUALS','WHILE','DO','FOR','STDIN','STDOUT','COMMENT','NOTEQUALS','TRUE','FALSE','DEFINE','FUNC', 'CALL','ELSE','PAL']
 
-literals=('=', '+','-','*','/','%', ';', '!', '>','<',')','(','{','}')
+literals=('=', '+','-','*','/','%', ';', '!', '>','<',')','(','{','}','++','--')
 
 #tenho de definir como funções
 t_INT = r'INT'
+t_ARRAY = r'ARRAY'
 t_IF = r'IF'
 t_IFELSE = r'IFELSE'
 t_ELSE = r'ELSE'
 t_WHILE = r'WHILE'
 t_DO = r'DO'
+t_FOR = r'FOR'
 t_CALL = r'CALL'
 t_DEFINE = r'DEFINE'
 t_TRUE = r'TRUE'
